946e3fad074d1e8e16a30b3513d20ec30fd4ee3b
Daniel Madalitso Phiri <malgamves@gmail.com>
2024-12-13 10:59:10 -0600

    added runtime

--- src/utils/collection.ts
+++ src/utils/collection.ts
@@ -1,6 +1,8 @@
 import client from "@/utils/weaviate"
 import { generative, vectorizer } from "weaviate-client";
 
+export const runtime = "edge";
+
 export default async function createCollection(collectionName: string) {
     await client.collections.create({
       name: collectionName,
